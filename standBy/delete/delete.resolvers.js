1abac5351cb443aead8879d9c4e90880e5340e4a
seojinwan <jinwanseo@gmail.com>
2022-11-27 22:05:13 +0900

    User > 대기 취소시, 다른 대기열 목록 없으면 유저 삭제

--- standBy/delete/delete.resolvers.js
+++ standBy/delete/delete.resolvers.js
@@ -29,9 +29,19 @@ export default {
               "대기열 삭제 불가 (대기열 삭제는 해당 대기열 등록 디바이스에서만 가능)"
             );
 
+          // 대기 정보 삭제
           await client.standBy.delete({
             where: { id: searchStandBy.id },
           });
+
+          // 유저 내 대기 리스트 없을시 유저도 삭제
+          const standByCount = await client.standBy.count({
+            where: {
+              userId: loggedInUser.id,
+            },
+          });
+          if (!standByCount)
+            await client.user.delete({ where: { id: loggedInUser.id } });
 
           return {
             ok: true,
