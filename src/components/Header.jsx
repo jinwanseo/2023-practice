40eb7dd492f59f149099ccc342d1c7550871cc81
seojinwan <jinwanseo@gmail.com>
2022-12-21 23:45:14 +0900

    -

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,35 @@ import { darkModeVar, disableDarkMode, enableDarkMode } from "apollo";
 import styled from "styled-components";
 import { LogoTitle } from "./Heading";
 import IconButton from "./IconButton";
+import LogoText from "./LogoText";
 
-export const Header = styled.div`
+const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const Header = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
   justify-content: space-between;
   align-items: center;
+  max-width: 960px;
 `;
 
 export default function HeaderBar() {
   const isDarkMode = useReactiveVar(darkModeVar);
 
   return (
-    <Header>
-      <LogoTitle>Standby Togeter</LogoTitle>
-      <IconButton onClick={isDarkMode ? disableDarkMode : enableDarkMode}>
-        <FontAwesomeIcon icon={isDarkMode ? faMoon : faSun} size="lg" />
-      </IconButton>
-    </Header>
+    <HeaderWrapper>
+      <Header>
+        {/* <LogoTitle>Standby Togeter</LogoTitle> */}
+        <LogoText onlyLogo={true} />
+        <IconButton onClick={isDarkMode ? disableDarkMode : enableDarkMode}>
+          <FontAwesomeIcon icon={isDarkMode ? faMoon : faSun} size="lg" />
+        </IconButton>
+      </Header>
+    </HeaderWrapper>
   );
 }
