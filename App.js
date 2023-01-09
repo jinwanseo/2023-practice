5fd00464524bf063e7778c77f6cb6aa9c05d9d13
seojinwan <jinwanseo@gmail.com>
2023-01-09 09:15:35 +0900

    -

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import { IonIcons } from "@expo/vector-icons/Ionicons";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontToLoad = [IonIcons.font];
+    const fontPromise = fontToLoad.map((font) => Font.loadAsync(font));
+    console.log("Hello React JS!");
+    return Promise.all([...fontPromise]);
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
